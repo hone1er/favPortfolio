8338227ec6bac1126d3b5acba5424fbed41282b1
Joe <josephmvillavicencio@gmail.com>
2021-12-06 15:03:53 -0800

    about page mockup complete

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,20 @@ import Link from "next/link";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="w-full flex flex-row items-center justify-between p-4 font-serif">
+      <div className="w-full flex flex-row items-center justify-between p-4 font-serif shadow">
         <h1 className=" text-2xl sm:text-3xl font-bold text-">
           <Link href="/">Favian Audieri</Link>
         </h1>
-        <ul className="flex flex-row">
-          <li className="p-2 text-sm sm:text-lg font-semibold">
+        <ul className="flex flex-col sm:flex-row  border-l-2 sm:border-none pl-2 sm:pl-0">
+          <li className="p-2 text-base sm:text-lg font-semibold">
             {" "}
             <Link href="/gallery">Gallery</Link>
           </li>
-          <li className="p-2 text-sm sm:text-lg font-semibold">
+          <li className="p-2 text-base sm:text-lg font-semibold">
             {" "}
-            <Link href="/">About</Link>
+            <Link href="/about">About</Link>
           </li>
-          <li className="p-2 text-sm sm:text-lg font-semibold">
+          <li className="p-2 text-base sm:text-lg font-semibold">
             {" "}
             <Link href="/">Contact</Link>
           </li>
--- pages/gallery.js
+++ pages/gallery.js
@@ -17,105 +17,127 @@ export default function Gallery() {
       </Head>
       <div className="container m-auto w-full font-serif">
         <h1 className="text-center text-6xl font-light mt-12 mb-8">Gallery</h1>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 gap-2">
           <Image
-            className="object-center object-cover"
+            className="object-center "
             loader={myLoader}
-            src={photo}
+            src="https://source.unsplash.com/random/425x425"
+            width={500}
+            height={500}
             alt="check"
             placeholder="blurDataURL"
           />
           <Image
-            className="object-center object-cover"
+            className="object-center "
             loader={myLoader}
-            src={photo}
+            src="https://source.unsplash.com/random/300x300"
+            width={500}
+            height={500}
             alt="check"
             placeholder="blurDataURL"
           />
           <Image
-            className="object-center object-cover"
+            className="object-center "
             loader={myLoader}
-            src={photo}
+            src="https://source.unsplash.com/random/250x250"
+            width={500}
+            height={500}
             alt="check"
             placeholder="blurDataURL"
           />
           <Image
-            className="object-center object-cover"
+            className="object-center "
             loader={myLoader}
-            src={photo}
+            src="https://source.unsplash.com/random/570x570"
+            width={500}
+            height={500}
             alt="check"
             placeholder="blurDataURL"
           />
         </div>
+        <br />
+        <br />
+        <br />
+        <br />
         <div className="flex flex-row flex-wrap flex-grow items-center justify-center">
-          <div className="cursor-pointer">
+          <div className="cursor-pointer m-2 p-6 bg-gradient-to-r from-gray-200 via-gray-100 to-gray-200 rounded-lg ">
             <Link href="">
               <Image
-                className="object-center object-cover h-auto"
+                className="object-center  h-auto"
                 loader={myLoader}
-                src={photo}
+                src="https://source.unsplash.com/random/400x400"
+                width={500}
+                height={500}
                 alt="check"
                 placeholder="blurDataURL"
               />
             </Link>
             <Link href="">
-              <p className="text-center">Business Cards</p>
+              <p className="text-center pt-4 ">Business Cards</p>
             </Link>
           </div>
-          <div className="cursor-pointer">
+          <div className="cursor-pointer m-2 p-6 bg-gradient-to-r from-gray-200 via-gray-100 to-gray-200 rounded-lg">
             <Link href="">
               <Image
-                className="object-center object-cover h-auto"
+                className="object-center  h-auto"
                 loader={myLoader}
-                src={photo}
+                src="https://source.unsplash.com/random/600x600"
+                width={500}
+                height={500}
                 alt="check"
                 placeholder="blurDataURL"
               />
             </Link>
             <Link href="">
-              <p className="text-center">Promotional Material</p>
+              <p className="text-center pt-4 ">Promotional Material</p>
             </Link>
           </div>
-          <div className="cursor-pointer">
+          <div className="cursor-pointer m-2 p-6 bg-gradient-to-r from-gray-200 via-gray-100 to-gray-200 rounded-lg ">
             <Link href="">
               <Image
-                className="object-center object-cover h-auto"
+                className="object-center  h-auto"
                 loader={myLoader}
-                src={photo}
+                src="https://source.unsplash.com/random/500x500"
+                width={500}
+                height={500}
                 alt="check"
                 placeholder="blurDataURL"
               />
             </Link>
             <Link href="">
-              <p className="text-center">Digital Art</p>
+              <p className="text-center pt-4 ">Digital Art</p>
             </Link>
           </div>
-          <div className="cursor-pointer">
+          <div className="cursor-pointer m-2 p-6 bg-gradient-to-r from-gray-200 via-gray-100 to-gray-200 rounded-lg ">
             <Link href="">
               <Image
-                className="object-center object-cover h-auto"
+                className="object-center  h-auto"
                 loader={myLoader}
-                src={photo}
+                src="https://source.unsplash.com/random/550x550"
+                width={500}
+                height={500}
                 alt="check"
                 placeholder="blurDataURL"
               />
             </Link>
             <Link href="">
-              <p className="text-center">Physical Art</p>
+              <p className="text-center pt-4 ">Physical Art</p>
             </Link>
           </div>
-          <div className="cursor-pointer">
+          <div className="cursor-pointer m-2 p-6 bg-gradient-to-r from-gray-200 via-gray-100 to-gray-200 rounded-lg ">
             <Link href="">
               <Image
-                className="object-center object-cover h-auto"
+                className="object-center  h-auto"
                 loader={myLoader}
-                src={photo}
+                src="https://source.unsplash.com/random/525x525"
+                width={500}
+                height={500}
                 alt="check"
                 placeholder="blurDataURL"
               />
             </Link>
             <Link href="">
-              <p className="text-center">Photos</p>
+              <p className="text-center pt-4 ">Photos</p>
             </Link>
           </div>
         </div>
